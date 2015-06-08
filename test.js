97f19f418cafe159ab6d54f254e6704226529d41
Joe Hitchens <joe@sleepless.com>
2015-06-08 11:48:35 -0700

    -

---
+++ test.js
@@ -0,0 +1,14 @@
+
+httpd = require('http').createServer(function(req, res) {
+	res.writeHead(200);
+	res.end(require("fs").readFileSync("./test.html");
+});
+
+require("./sleepless-ws.js").listen(httpd, function(new_client) {
+
+	new_client.accept(function(m) {
+		m.reply("you said: "+m.msg);
+	}) 
+
+})
+
